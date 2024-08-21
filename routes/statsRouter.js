df520ad3281e156e877407944ee06bc72e978112
jackavryashnik <jackavryashnik@gmail.com>
2024-08-21 18:20:17 +0200

    add calculating of total and current stats

--- routes/statsRouter.js
+++ routes/statsRouter.js
@@ -1,3 +1,4 @@
+import calculateCurrentGuests from '../helpers/calculateCurrentGuests.js';
 import Shelter from '../schemas/shelterModel.js';
 
 const handleSocketConnection = async socket => {
@@ -7,16 +8,25 @@ const handleSocketConnection = async socket => {
 
   socket.on('updateRoom', async ({ roomId, bedsTaken, beds }) => {
     if (roomId || bedsTaken || beds) {
-      const result = await Shelter.updateOne(
-        { _id: '668e446687a20aa914d196b6', 'rooms._id': roomId },
-        { $set: { 'rooms.$.bedsTaken': bedsTaken, 'rooms.$.beds': beds } }
-      );
+      const room = stats.rooms.find(room => room._id.toString() === roomId);
 
-      if (result.modifiedCount > 0) {
-        const updatedStats = await Shelter.findOne();
-        socket.broadcast.emit('bedsFetched', updatedStats);
-        socket.emit('bedsFetched', updatedStats);
+      if (room) {
+        const currentlyBedsTaken = room.bedsTaken;
+
+        if (bedsTaken > currentlyBedsTaken) {
+          stats.totalGuests += bedsTaken - currentlyBedsTaken;
+        }
+
+        room.bedsTaken = bedsTaken;
+        room.beds = beds;
       }
+
+      stats.currentGuests = calculateCurrentGuests(stats.rooms);
+
+      await stats.save();
+
+      socket.emit('bedsFetched', stats);
+      socket.broadcast.emit('bedsFetched', stats);
     }
   });
 };
